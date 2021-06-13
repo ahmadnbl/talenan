06314fcdb4ad1ff50d408b0fc483e8706e35eb9d
Ahmad Nabili <ahmad.nabili@tokopedia.com>
2021-06-13 21:42:30 +0700

    Initial Commit

---
+++ Talenan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Talenan
+//
+//  Created by Ahmad Nabili on 13/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
